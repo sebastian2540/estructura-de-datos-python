ed74e7195ca62f125fa3e1ea013aa05dd368da5e
CESDE <kellyfuentes2007@gmail.com>
2024-03-15 18:41:06 -0500

    firts commit python

---
+++ listas.py
@@ -0,0 +1,12 @@
+# Estructura de datos
+#Son objetos que permiten almacenar en una sola variable.
+
+fruta1 = "Manzana"
+fruta2 = "Mango"
+frutas = ["Manzana","Mango","Fresa"]
+
+#Listas
+"""
+Listas = []
+Permite cualquier tipo de datos
+"""
