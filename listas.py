61b20ef84bd010dcd8c4bfbd4cadad101ca2f133
sebastian.villada.jaramillo@gmail.com <kellyfuentes2007@gmail.com>
2024-03-15 19:34:31 -0500

    first commit

--- listas.py
+++ listas.py
@@ -15,7 +15,7 @@ Son indexadas
 #Atributos
 #len(), pop(), remove(), clear(), index(), del, sort(), reverse, extende(), zip()
 
-deportes = ["futbol","tenis","basquetboll","voleibol","patinaje","natación"]
+"""deportes = ["futbol","tenis","basquetboll","voleibol","patinaje","natación"]
 #cantidad de elementos de la lista: len()
 print(len(deportes))
 
@@ -35,5 +35,21 @@ deportes.insert(2, "golf")
 print(deportes)
 #modificar o cambiar un deporte
 deportes[2] = "esgrima"
-print(deportes)
-#crear 2 lista, una vacia y otra lista numerica. Validar que numeros son impares y agregalos a la lista vacia.
+print(deportes)"""
+#crear 2 lista, una vacia y otra lista numerica. Validar que numeros son impares y agregalos a la lista vacia. Mostrar lista con los numeros impares
+
+impares = []
+numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15,17,18,19,20]
+print(len(numeros))
+
+for n in numeros:
+    if n % 2 !=0:
+        impares.append(n)
+print(impares)
+
+#Eliminar elementos
+#pop(indice): Elimina elementos en la posicion especificada
+#pov(): elimina el ultimo elemento y muestra el elemento eliminado
+#remove(elemento): elimna elemento pasado como argumento
+#clear(): elimina los elementos de la lista
+#del nombre_lista: elimina el objeto, es decir, la lista.
